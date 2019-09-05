3f7c2e20687892e853aad18be452136a556953e3
koshian2 <saltcoffeehorse@gmail.com>
2019-09-05 22:02:26 +0900

    change spectral norm

--- models/basic_module.py
+++ models/basic_module.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
-from torch.nn.utils.spectral_norm import spectral_norm
+from torch.nn import Parameter
 
 def init_xavier_uniform(layer):
     if hasattr(layer, "weight"):
@@ -9,6 +9,65 @@ def init_xavier_uniform(layer):
     if hasattr(layer, "bias"):
         if hasattr(layer.bias, "data"):       
             layer.bias.data.fill_(0)
+
+# SPectral Norm
+# https://github.com/ajbrock/BigGAN-PyTorch/blob/master/TFHub/biggan_v1.py
+
+def l2normalize(v, eps=1e-4):
+	return v / (v.norm() + eps)
+
+class SpectralNorm(nn.Module):
+	def __init__(self, module, name='weight', power_iterations=1):
+		super(SpectralNorm, self).__init__()
+		self.module = module
+		self.name = name
+		self.power_iterations = power_iterations
+		if not self._made_params():
+			self._make_params()
+
+	def _update_u_v(self):
+		u = getattr(self.module, self.name + "_u")
+		v = getattr(self.module, self.name + "_v")
+		w = getattr(self.module, self.name + "_bar")
+
+		height = w.data.shape[0]
+		_w = w.view(height, -1)
+		for _ in range(self.power_iterations):
+			v = l2normalize(torch.matmul(_w.t(), u))
+			u = l2normalize(torch.matmul(_w, v))
+
+		sigma = u.dot((_w).mv(v))
+		setattr(self.module, self.name, w / sigma.expand_as(w))
+
+	def _made_params(self):
+		try:
+			getattr(self.module, self.name + "_u")
+			getattr(self.module, self.name + "_v")
+			getattr(self.module, self.name + "_bar")
+			return True
+		except AttributeError:
+			return False
+
+	def _make_params(self):
+		w = getattr(self.module, self.name)
+
+		height = w.data.shape[0]
+		width = w.view(height, -1).data.shape[1]
+
+		u = Parameter(w.data.new(height).normal_(0, 1), requires_grad=False)
+		v = Parameter(w.data.new(height).normal_(0, 1), requires_grad=False)
+		u.data = l2normalize(u.data)
+		v.data = l2normalize(v.data)
+		w_bar = Parameter(w.data)
+
+		del self.module._parameters[self.name]
+		self.module.register_parameter(self.name + "_u", u)
+		self.module.register_parameter(self.name + "_v", v)
+		self.module.register_parameter(self.name + "_bar", w_bar)
+
+	def forward(self, *args):
+		self._update_u_v()
+		return self.module.forward(*args)
 
 class ConditionalBatchNorm2d(nn.Module):
   def __init__(self, num_features, num_classes, eps=1e-4, momentum=0.1):
@@ -32,7 +91,7 @@ class ConditionalBatchNorm2d(nn.Module):
 class SNConv2d(nn.Module):
     def __init__(self, in_ch, out_ch, kernel_size, stride=1, padding=0):
         super().__init__()
-        self.layer = spectral_norm(nn.Conv2d(in_ch, out_ch, kernel_size, stride=stride, padding=padding))
+        self.layer = SpectralNorm(nn.Conv2d(in_ch, out_ch, kernel_size, stride=stride, padding=padding))
         self.layer.apply(init_xavier_uniform)
 
     def forward(self, inputs):
@@ -41,7 +100,7 @@ class SNConv2d(nn.Module):
 class SNLinear(nn.Module):
     def __init__(self, in_dims, out_dims, bias=True):
         super().__init__()
-        self.layer = spectral_norm(nn.Linear(in_dims, out_dims, bias=bias))
+        self.layer = SpectralNorm(nn.Linear(in_dims, out_dims, bias=bias))
         self.layer.apply(init_xavier_uniform)
 
     def forward(self, inputs):
